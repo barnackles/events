9536a07a7986905bf18a2e9fa76d8a9758cfd748
barnackles <p.martewicz@yahoo.com>
2023-05-04 19:02:07 +0200

    ApplicationEventPublisherAware implemented by ProductController. Added publisher field.

--- src/main/java/com/kodilla/events/controller/ProductController.java
+++ src/main/java/com/kodilla/events/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.kodilla.events.controller;
 
 import com.kodilla.events.domain.ProductDto;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/products")
-public class ProductController {
+public class ProductController implements ApplicationEventPublisherAware {
 
+    private ApplicationEventPublisher publisher;
     @PostMapping(path = "createProduct")
     public void createProduct(@RequestBody ProductDto productDto) {
         System.out.println("Register product: " + productDto.getProductName());
     }
 
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.publisher = applicationEventPublisher;
+    }
 }
